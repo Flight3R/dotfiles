618d04efd674b05360ff8099317352c5b871d157
Flight3R <blaszczyna@gmail.com>
2026-08-09 13:03:39 +0200

    trim notam, cd vim

--- .config/nvim/lua/mapping.lua
+++ .config/nvim/lua/mapping.lua
@@ -5,13 +5,14 @@ local live_grep = require("telescope").extensions.live_grep_args
 vim.keymap.set("n", "fg", function() live_grep.live_grep_args() end, { desc = "live grep" })
 vim.keymap.set("n",	"gr", builtin.lsp_references, { desc = "show lsp references" })
 vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "go to definition" })
-
+vim.keymap.set("n", "fd", ":Telescope projects<CR>", { desc = "show projects" })
+vim.keymap.set("n", "tg",":GitBlameToggle<CR>", { desc = "toggle git blame log line, turn on / off" })
+vim.keymap.set("n", "gb", ":GitBlameOpenCommitURL<CR>", { desc = "open git blame commit in browser" })
+vim.keymap.set("n", "yh", ":Telescope yank_history<CR>", { desc = "telescope yank history" })
 -- exit from file
 vim.keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true, silent = true, desc = "Exit from file" })
-
 -- toggle line wrap
 vim.keymap.set("n", "<leader>z", ":set wrap!<CR>", { noremap = true, silent = true, desc = "Toggle line wrap" })
-
 -- Dont yank with x or d or c
 vim.keymap.set('n', 'x', '"_x')
 vim.keymap.set('n', 'X', '"_X')
@@ -20,36 +21,25 @@ vim.keymap.set('n', 'C', '"_C')
 -- also for visual mode
 vim.keymap.set('v', 'c', '"_c')
 vim.keymap.set('v', 'x', '"_x')
-
 -- copy to system clipboard
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy selected area to system clipboard" })
 vim.keymap.set("n", "<leader>Y", ":%yank +<CR>", { desc = "Copy whole file to system clipboard" })
-
 -- show diff
-vim.keymap.set("n", "di", [[:w !diff % -<CR>]], { noremap = true, silent = true, desc = "Show diff" })
-
+vim.keymap.set("n", "<leader>di", [[:w !diff % -<CR>]], { noremap = true, silent = true, desc = "Show diff" })
 -- show diag
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics" })
-
+-- cd
+vim.keymap.set("n", "<leader>cd", ":cd %:p:h<CR>", { desc = "cd to current file directory" })
 -- buffer control (switching between files)
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>")
 vim.keymap.set("n", "<leader>q", ":bdelete<CR>")
 vim.keymap.set("n", "<leader><Tab>", ":Telescope buffers<CR>")
-
 vim.keymap.set("n", "<leader>n", ":vnew<CR>", { noremap = true, silent = true, desc = "Open new buffer" })
-
-vim.keymap.set("n", "rp", function()
+-- realpath
+vim.keymap.set("n", "<leader>rp", function()
   local file = vim.api.nvim_buf_get_name(0)
   local path = vim.trim(vim.fn.system({ "realpath", file }))
   vim.fn.setreg("+", path)
   vim.notify("Copied path: " .. path)
 end, { desc = "Copy realpath of current file to system clipboard" })
-
-vim.keymap.set("n", "fd", ":Telescope projects<CR>", { desc = "show projects" })
-
-vim.keymap.set("n", "tg",":GitBlameToggle<CR>", { desc = "toggle git blame log line, turn on / off" })
-
-vim.keymap.set("n", "gb", ":GitBlameOpenCommitURL<CR>", { desc = "open git blame commit in browser" })
-
-vim.keymap.set("n", "yh", ":Telescope yank_history<CR>", { desc = "telescope yank history" })
